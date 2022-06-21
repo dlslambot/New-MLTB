383ecce0df9c8ea053088d8d9221a776c94754a0
dlslambot <106543696+dlslambot@users.noreply.github.com>
2022-06-21 16:20:26 +0530

    Update __main__.py

--- bot/__main__.py
+++ bot/__main__.py
@@ -65,8 +65,8 @@ def stats(update, context):
 
 def start(update, context):
     buttons = ButtonMaker()
-    buttons.buildbutton("Repo", "https://www.github.com/anasty17/mirror-leech-telegram-bot")
-    buttons.buildbutton("Report Group", "https://t.me/+PRRzqHd31XY3ZWZk")
+    buttons.buildbutton("⚡Mirror-Leech Group⚡", "https://t.me/+cvPJqSAyHuNmNzk1")
+    buttons.buildbutton("⚡Owner⚡", "https://t.me/Dark_LordSl")
     reply_markup = InlineKeyboardMarkup(buttons.build_menu(2))
     if CustomFilters.authorized_user(update) or CustomFilters.authorized_chat(update):
         start_string = f'''
