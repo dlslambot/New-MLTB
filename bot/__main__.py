d852790b03bacd514d20751ad249ad84c630e58c
dlslambot <106543696+dlslambot@users.noreply.github.com>
2022-06-21 16:27:58 +0530

    Update __main__.py

--- bot/__main__.py
+++ bot/__main__.py
@@ -75,7 +75,7 @@ Type /{BotCommands.HelpCommand} to get a list of available commands
 '''
         sendMarkup(start_string, context.bot, update.message, reply_markup)
     else:
-        sendMarkup('Not Authorized user, deploy your own mirror-leech bot', context.bot, update.message, reply_markup)
+        sendMarkup('Not An Authorized User, Use The Group To Use Me', context.bot, update.message, reply_markup)
 
 def restart(update, context):
     restart_message = sendMessage("Restarting...", context.bot, update.message)
